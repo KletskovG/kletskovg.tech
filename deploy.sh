156ecae3db72c808bfc27b404d4def0d71a3643f
Gleb Kletskov <gfgfddgleb@gmail.com>
2019-10-21 20:19:41 +0300

    First commit

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -xe
+
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+
+  git init
+
+  git remote add deploy "travis@kletskovg.tech:/var/www/kletskovg.tech"
+  git config user.name "Travis CI"
+  git config user.email "gfgfddglebrtravis@gmail.com"
+
+  git add .
+  git commit -m "Deploy"
+  git push --force deploy master
+else
+  echo "Not deploying, since this branch isn't master."
+fi
