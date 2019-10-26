cf52d0f4967da60cbcd776e28240b648fb17aca2
Gleb Kletskov <gfgfddgleb@gmail.com>
2019-10-26 17:31:42 +0300

    Add server routing

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,22 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist/pages/index.html'));
 });
 
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/pages/blog.html'));
+});
+
+app.get('/about-me', (req, res) => {
+   res.sendFile(path.join(__dirname, '../../client/dist/pages/about-me.html'));
+});
+
+app.get('/projects', (req, res) => {
+   res.sendFile(path.join(__dirname, '../../client/dist/pages/projects.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/pages/contact.html'));
+});
+
 
 app.listen(8080, () => {
     console.log('Now you are listening to port number 8080');
