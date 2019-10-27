9343dce939090de12c530fe6a9f8d7891be7c5fd
Gleb Kletskov <gfgfddgleb@gmail.com>
2019-10-27 16:21:47 +0300

    Update contact and about pages

--- client/entries/images.ts
+++ client/entries/images.ts
@@ -11,4 +11,4 @@ import '../src/assets/img/imagesimagesimage9.jpg';
 import '../src/assets/img/imagesimagestest.jpg';
 import '../src/assets/img/TelegramContest.png';
 import '../src/assets/img/FaceRecog.jpg';
-import '../src/assets/img/codeWallpaper.jpeg';
+import '../src/assets/img/codeWallpaper.jpeg';import '../src/assets/img/avatar2.png'; 
