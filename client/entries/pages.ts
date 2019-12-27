0689c00bdd6a40862bdc5732006ae741e40bd130
Gleb Kletskov <gfgfddgleb@gmail.com>
2019-12-27 13:52:21 +0300

    Rename client folder

--- client/entries/pages.ts
+++
@@ -1,6 +0,0 @@
-import '../src/pages/index.html';
-import '../src/pages/blog.html'; 
-import '../src/pages/contact.html'; 
-import '../src/pages/about-me.html'; 
-import '../src/pages/projects.html'; 
-import '../src/pages/file.html'; 
--- client/entries/scripts.ts
+++
@@ -1,6 +0,0 @@
-import '../src/scripts/setImages.js';
-import '../src/scripts/menuButton.js'; 
-import '../src/scripts/getLatestCommit.js';
-import '../src/scripts/sendEmail.js'; 
-import '../src/scripts/svgTravelling.js'; 
-import '../src/scripts/sendFile.js'; 
