8472ea86c95e6ebb795fd16832cfba36ceaa687e
Gleb Kletskov <gfgfddgleb@gmail.com>
2019-12-28 21:17:57 +0300

    #1 implemented

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface gitLink {
+  message: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'kletskovg.tech';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient){}
+
+  public title: string  = 'kletskovg.tech';
+  public githubLink: string;
+
+  ngOnInit() {
+    this.http.get<gitLink>('http://localhost:3200/gitlink', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'mode': 'no-cors',
+      },
+    })
+     .subscribe(
+        value => this.githubLink = value.message,
+        error => console.log(error),
+      );
+  }
 }
