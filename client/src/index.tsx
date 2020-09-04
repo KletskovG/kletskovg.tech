4ce4421f0f7b36964652358f5af29d05a0ca3d5f
Gleb Kletskov <gfgfddgleb@gmail.com>
2020-09-04 10:08:26 +0300

    init react project

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { store } from './store/index';
+import { ThemeProvider } from 'emotion-theming';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={store.getState().theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Make it register if you want to use service worker
 serviceWorker.unregister();
