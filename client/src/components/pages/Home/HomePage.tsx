329cccd4ccd3f992e436f9dbe699470750aa3ee3
Gleb Kletskov <gfgfddgleb@gmail.com>
2020-09-09 00:15:58 +0300

    add loader animation

--- client/src/components/pages/Home/HomePage.tsx
+++ client/src/components/pages/Home/HomePage.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store/index';
-import { css } from '@emotion/core';
+import { css, keyframes } from '@emotion/core';
 import { useLocation } from 'wouter';
 import Page from '../../layout/Page';
 import Rightside from './Rightside';
 import Leftside from './Leftside';
+import TitleLoader from './TitleLoader';
 
 const HomePage = () => {
   const [, setLocation] = useLocation();
   const theme = useSelector((state: RootState) => state.theme);
+
+  const [isTitle, setisTitle] = useState(true);
+
+  const home = keyframes`
+    0% {
+      opacity: 0;
+    }
+
+    80% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  `;
+
   const homeStyles = css`
     display: flex;
     justify-content: space-between;
@@ -17,6 +35,7 @@ const HomePage = () => {
     width: 100vw;
     position: relative;
     padding-top: 3%;
+    animation: ${home} 1.7s ease 1 forwards;
 
     .job-title {
       width: 50%;
@@ -24,12 +43,24 @@ const HomePage = () => {
     }
   `;
 
+  useEffect(() => {
+    setTimeout(() => {
+      setisTitle(false);
+    }, 1700);
+  }, []);
+
   return (
     <Page>
-      <div className="home-page" css={homeStyles}>
-        <Rightside />
-        <Leftside />
-      </div>
+        {
+          isTitle ?
+          <TitleLoader />
+          :
+          <div className="home-page" css={homeStyles}>
+            <Rightside />
+            <Leftside />
+          </div>
+        }
+      
     </Page>
   )
 }
