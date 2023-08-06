a99bc02dda7738c1b43637590019cfd3331c49f6
Kletskov Gleb <gfgfddgleb@gmail.com>
2023-08-06 22:29:48 +0200

    Update test v3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Gleb Kletskov personal website UPD v2</h1>
+        <h1>Gleb Kletskov personal website UPD v3</h1>
       </header>
     </div>
   );
