f892c16f82a1a9dd718f892cc9c15b9508c3aa71
Kletskov Gleb <gfgfddgleb@gmail.com>
2023-12-10 19:30:34 +0000

    fix: api url

--- server/src/const/budget.ts
+++ server/src/const/budget.ts
@@ -1,2 +1,1 @@
-// export const BUDGET_SERVICE_API_URL = "https://api.kletskovg.tech";
-export const BUDGET_SERVICE_API_URL = "http://localhost:8080";
+export const BUDGET_SERVICE_API_URL = "https://api.kletskovg.tech";
